2bda1ae4fc57cfba66dc9bc62da89dc380582065
Yuval Cohen <=>
2023-06-06 17:51:02 +0300

    Update user_data of ec2 instance and apply

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -13,27 +13,30 @@ resource "aws_instance" "es_kibana_instance" {
   tags = {
     Name = "ESKibana"
   }
-
+  # editing and then running apply will create a new instance
   user_data = <<-EOF
               #!/bin/bash
+              echo "Setting up docker"
               sudo yum update -y
               sudo yum install -y docker
               sudo service docker start
               sudo usermod -a -G docker ec2-user
+              sudo systemctl enable docker
+              sudo chkconfig docker on
 
               # Pull the images
+              echo "Pulling images"
               sudo docker pull docker.elastic.co/kibana/kibana:7.10.2
               sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:7.10.2
 
               # Start the containers
-              sudo docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -v es_data:/usr/share/elasticsearch/data docker.elastic.co/elasticsearch/elasticsearch:7.10.2
-              sudo docker run -d --name kibana --link elasticsearch:elasticsearch -p 5601:5601 -v kibana_data:/usr/share/kibana/data docker.elastic.co/kibana/kibana:7.10.2
+              echo "Starting containers"
+              sudo docker run -d --name elasticsearch -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --restart always -v es_data:/usr/share/elasticsearch/data docker.elastic.co/elasticsearch/elasticsearch:7.10.2
+              sudo docker run -d --name kibana --link elasticsearch:elasticsearch -p 5601:5601 --restart always -v kibana_data:/usr/share/kibana/data docker.elastic.co/kibana/kibana:7.10.2
 
               # Install plugin
+              echo "Installing plugin"
               sudo docker exec elasticsearch ./bin/elasticsearch-plugin install --batch https://github.com/Immanuelbh/elasticsearch-analysis-hebrew/releases/download/elasticsearch-analysis-hebrew-7.10.2/elasticsearch-analysis-hebrew-7.10.2.zip
-
-              # Verify installation
-              sudo docker exec elasticsearch ./bin/elasticsearch-plugin list
 
               # Restart elasticsearch and kibana
               sudo docker restart elasticsearch kibana
